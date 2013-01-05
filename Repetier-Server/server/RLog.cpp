e9a0772c61f0ad9d720dae6f5cb4cb3a45c91c11
repetier <darko371@googlemail.com>
2013-01-05 13:45:57 +0100

    Logging into syslog / better serial initialization

---
+++ Repetier-Server/server/RLog.cpp
@@ -0,0 +1,9 @@
+//
+//  RLog.cpp
+//  Repetier-Server
+//
+//  Created by Roland Littwin on 03.01.13.
+//
+//
+
+#include "RLog.h"
---
+++ Repetier-Server/server/RLog.h
@@ -0,0 +1,14 @@
+//
+//  RLog.h
+//  Repetier-Server
+//
+//  Created by Roland Littwin on 03.01.13.
+//
+//
+
+#ifndef __Repetier_Server__RLog__
+#define __Repetier_Server__RLog__
+
+#include <iostream>
+
+#endif /* defined(__Repetier_Server__RLog__) */
